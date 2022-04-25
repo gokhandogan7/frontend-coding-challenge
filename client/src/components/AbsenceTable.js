3fa4eafdf6d732453ed788c5e399922c4172ac4e
Gokhan_D <gdogan3746@gmail.com>
2022-04-26 01:02:15 +0200

    feature/pagination implemented

--- client/src/components/AbsenceTable.js
+++ client/src/components/AbsenceTable.js
@@ -1,5 +1,5 @@
 import React, { useMemo, useEffect, useState } from "react";
-import { useTable, useFilters } from "react-table";
+import { useTable, useFilters, usePagination } from "react-table";
 import { COLUMNS } from "./columns";
 import "./table.css";
 import { absenceServices } from "../services/absenceService";
@@ -26,52 +26,117 @@ export const AbsenceTable = () => {
     fetchData();
   }, []);
 
-  const tableInstance = useTable({
-    columns,
-    data: absences,
-  },
-  useFilters
+  const tableInstance = useTable(
+    {
+      columns,
+      data: absences,
+    },
+    useFilters,
+    usePagination
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance;
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    state,
+    gotoPage,
+    pageCount,
+    prepareRow,
+  } = tableInstance;
+  console.log(absences.length);
+
+  const { pageIndex } = state;
 
   return (
     <div>
       {loading ? (
-        <BounceLoader color={'#fca138'} size={150} loading />
+        <BounceLoader color={"#fca138"} size={150} loading />
       ) : (
-        <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>
-                  {column.render("Header")}
-                  <div>
-                    {" "}
-                    {column.canFilter ? column.render("Filter") : null}{" "}
-                  </div>
-                </th>
+        <>
+          <table {...getTableProps()}>
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <th {...column.getHeaderProps()}>
+                      {column.render("Header")}
+                      <div>
+                        {" "}
+                        {column.canFilter ? column.render("Filter") : null}{" "}
+                      </div>
+                    </th>
+                  ))}
+                </tr>
               ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row, index) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {page.map((row, index) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell) => {
+                      return (
+                        <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <div>
+            <span>
+              Page{""}
+              <strong>
+                {pageIndex + 1} of {pageOptions.length}
+              </strong>
+            </span>
+            <span>
+              Results{""}
+              <strong>
+                {page.length} of {absences.length}
+              </strong>
+            </span>
+            <span>
+              | Goaa to Page : {""}
+              <input
+                type="number"
+                min="1"
+                max={pageOptions.length}
+                defaultValue={pageIndex + 1}
+                onChange={(e) => {
+                  const pageNumber = e.target.value
+                    ? Number(e.target.value) - 1
+                    : 0;
+                  gotoPage(pageNumber);
+                }}
+                style={{ width: 50 }}
+              />
+            </span>
+            <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+              {"<<"}
+            </button>
+            <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+              Previous
+            </button>
+            <button onClick={() => nextPage()} disabled={!canNextPage}>
+              Next
+            </button>
+            <button
+              onClick={() => gotoPage(pageCount - 1)}
+              disabled={!canNextPage}
+            >
+              {">>"}
+            </button>
+          </div>
+        </>
       )}
     </div>
   );
