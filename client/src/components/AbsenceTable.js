7e9f25207a6831f5ec63ed09f8423dfd5f5627e0
Gokhan_D <gdogan3746@gmail.com>
2022-04-25 23:15:41 +0200

    folder structure created and absence table implemented

---
+++ client/src/components/AbsenceTable.js
@@ -0,0 +1,72 @@
+import React, { useMemo, useEffect, useState } from "react";
+import { useTable } from "react-table";
+import { COLUMNS } from "./columns";
+import "./table.css";
+import { absenceServices } from "../services/absenceService";
+import { BounceLoader } from "react-spinners";
+
+export const AbsenceTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const [absences, setAbsences] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const data = await absenceServices.getAllAbsences();
+      console.log(data);
+      setLoading(false);
+      setAbsences(data.payload);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+  console.log(loading, absences);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const tableInstance = useTable({
+    columns,
+    data: absences,
+  });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  return (
+    <div>
+      {loading ? (
+        <BounceLoader color={'#fca138'} size={150} loading />
+      ) : (
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, index) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
---
+++ client/src/components/columns.js
@@ -0,0 +1,31 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Name',
+        accessor: 'userName'
+    },
+    {
+        Header: 'Type of Absence',
+        accessor: 'type'
+    },
+    {
+        Header: 'Period',
+        accessor:'period'
+    },
+    {
+        Header: 'Member Note',
+        accessor: 'memberNote'
+    },
+    {
+        Header: 'Status',
+        accessor:'status'
+    },
+    {
+        Header: 'Admitter Note',
+        accessor:'admitterNote'
+    },
+]
+
