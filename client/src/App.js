adbd903531822b4bab252e6df335693a83d23ea2
Gokhan_D <gdogan3746@gmail.com>
2022-04-26 13:53:00 +0200

    feature/ react router dom installed and home page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import { AbsenceTable } from "./components/AbsenceTable";
+import { HomePage } from "./components/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <AbsenceTable />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/absence" element={<AbsenceTable />}/>  
+        </Routes>
+      </Router>
     </div>
   );
 }
